7d9c54ce04bffff2bd4170ed199ceb7f916f0891
Yogin-Maniya <yoginmaniya76@gmail.com>
2025-10-02 10:03:15 +0530

    import

--- routes/User/questionRoutes.js
+++ routes/User/questionRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { sql, poolPromise } = require('../../db');
 const router = express.Router();
-
+const { decryptId } = require("../utils/encryption");
 /**
  * @swagger
  * /api/questions/{examId}:
